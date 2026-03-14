919f4b8c1faf7159c79e1d486a34fd921cb65d2f
Bryan_Baruk <bryanbaruck@gmail.com>
2026-03-14 19:37:46 -0300

    Criação e intrgração das Páginas HTML e JS

---
+++ script.js
@@ -0,0 +1,38 @@
+const produtos = [
+    {
+        nome: "Maça",
+        preco: "10.00",
+        quantidade: "15",
+        categoria: "fruta"
+    },
+    {
+        nome: "Banana",
+        preco: "8.50",
+        quantidade: "20",
+        categoria: "fruta"
+    },
+    {
+        nome: "Suco de Laranja",
+        preco: "12.00",
+        quantidade: "8",
+        categoria: "bebida"
+    },
+    {
+        nome: "Água Mineral",
+        preco: "3.50",
+        quantidade: "30",
+        categoria: "bebida"
+    },
+    {
+        nome: "Arroz",
+        preco: "25.90",
+        quantidade: "10",
+        categoria: "alimento"
+    },
+    {
+        nome: "Feijão",
+        preco: "12.80",
+        quantidade: "12",
+        categoria: "alimento"
+    }
+];
